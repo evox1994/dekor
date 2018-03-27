7d71b0199169b6ba69ed23584e2f9d40640753e1
evox1994 <dmitrybatukhtin@gmail.com>
2018-03-27 20:22:13 +0700

    sdfghjk

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        var st = $(this).scrollTop();
+        if ( st > 0 ) {
+            $('.header').css('background-color','#aa7d1e');
+        } else {
+            $('.header').css('background-color','transparent');
+        }
+    });
+
+    $('.mobile-btn').click(function(){
+        if ( $(this).hasClass("active") ) {
+            $('.mobile-menu').animate({height: 0},300);
+            setTimeout(function(){
+                $('.mobile-menu').removeClass("active");
+                $('.header').removeClass("active");
+                $('.mobile-btn').removeClass("active");
+                $('.mobile-background').removeClass("active");
+            }, 300);
+        } else {
+            $(this).addClass("active");
+            $('.mobile-background').addClass("active");
+            $('.header').addClass("active");
+            $('.mobile-menu').addClass("active");
+            $('.mobile-menu').animate({height: 298},300);
+        }
+    });
+
+    $('.mobile-background').click(function(){
+        $('.mobile-menu').animate({height: 0},300);
+        setTimeout(function(){
+            $('.mobile-menu').removeClass("active");
+            $('.header').removeClass("active");
+            $('.mobile-btn').removeClass("active");
+            $('.mobile-background').removeClass("active");
+        }, 300);
+    });
+
+});
